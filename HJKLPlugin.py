d9e3dcdc8c6a68217b1c55765e2621b12c271306
Rui Carmo <rui.carmo@gmail.com>
2013-04-01 20:09:48 +0100

    Added support for x (delete message) and v (view first URL in message)

--- HJKLPlugin.py
+++ HJKLPlugin.py
@@ -9,8 +9,21 @@ License: MIT (see LICENSE for details)
 
 from AppKit import *
 from Foundation import *
+from ScriptingBridge import *
 from Quartz.CoreGraphics import * # CGEvent
 import objc
+import email
+import subprocess
+from HTMLParser import HTMLParser
+
+class HREFParser(HTMLParser):
+    hrefs = []
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
+        if tag == 'a' and 'href' in attrs:
+            if attrs['href'].startswith('http'):
+                self.hrefs.append(attrs['href'])
+
 
 def swizzle(*args):
     """ Brilliant piece of coding from http://klep.name/programming/python/ """
@@ -27,11 +40,31 @@ def swizzle(*args):
     return decorator
 
 
+def view_first_link():
+    try:
+        mail_app = SBApplication.applicationWithBundleIdentifier_("com.apple.Mail")
+        source = mail_app.selection()[0].source()
+    except Exception, e:
+        NSLog('Could not obtain source of message. %s' % e)
+        return
+
+    p = HREFParser()
+    msg = email.message_from_string(source.encode('utf-8'))
+    for part in msg.walk():
+        if part.get_content_type() == 'text/html':
+            payload = part.get_payload(None, True).replace('\r','')
+            p.feed(payload)
+            if len(p.hrefs):
+                subprocess.call("open '%s'" % p.hrefs[0], shell=True)
+                return
+
+
 orthodox_keymap = {
     4:  123, # h maps to cursor left
     37: 124, # l maps to cursor right
     38: 125, # j maps to cursor up
-    40: 126  # k maps to cursor down
+    40: 126, # k maps to cursor down
+    7:  51   # x maps to backspace
 }
 
 
@@ -40,7 +73,8 @@ list_keymap = {
     4:  123, # h maps to cursor left
     37: 124, # l maps to cursor right
     38: 125, # j maps to cursor down
-    40: 126  # k maps to cursor up
+    40: 126, # k maps to cursor up
+    7:  51   # x maps to backspace
 }
 
 
@@ -49,14 +83,17 @@ msg_keymap = {
     4:  115, # h maps to Fn + cursor left
     37: 119, # l maps to Fn + cursor right
     38: 124, # j maps to cursor left  - next message 
-    40: 123  # k maps to cursor right - previous message
+    40: 123, # k maps to cursor right - previous message
+    7:  51   # x maps to backspace
 }
 
 
 def change_keyCode_(self, original, event, keymap):
     code = event.keyCode()
     #NSLog('Handling key %d' % code)
-    if code in keymap.keys():
+    if code == 9: # v
+        view_first_link()
+    elif code in keymap.keys():
         #NSLog("Changing key %d to %d" % (code, keymap[code]))
         original(self, NSEvent.eventWithCGEvent_(CGEventCreateKeyboardEvent(None,keymap[code],True)));
         return None # kill original event
